51f06d42ce194ffbd79188bc8276f01f10efe7e2
kurosetsuki <Bettycheng0801@gmail.com>
2023-05-09 15:32:40 +0800

    init

---
+++ a982181-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace a982181_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
